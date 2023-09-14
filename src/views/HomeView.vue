11f24e09cbb0dadf855962bdee3a6f069783e22b
caizb <caizhibin@cleartv.cn>
2023-09-14 16:48:03 +0800

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { onMounted } from 'vue'
+import data from './data.json'
+onMounted(() => {
+  console.log(data)
+})
+</script>
+
+<template>
+  <main>
+    <ul id="list">
+      <li v-for="(i, index) in data" :key="index"><a :href="i.link"><img :src="i.link" alt="3cx" height="50"></a>
+      </li>
+
+    </ul>
+  </main>
+</template>
