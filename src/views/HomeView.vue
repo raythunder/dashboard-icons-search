0e0917a183b7c57a2036f30cad8b319503c26af3
caizb <caizhibin@cleartv.cn>
2023-09-15 10:30:30 +0800

    feat: add hover tips,fix copy url

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -17,9 +17,10 @@ onMounted(() => {
     <input placeholder="input for search" class="w-full mb-3 input-bordered input" v-model="keywords" type="text">
 
     <RecycleScroller class="h-[calc(100vh-100px)]" :items="result" :item-size="100" key-field="text" v-slot="{ item }">
-      <a class="flex items-center justify-between transition border-gray-300 border-solid link link-info user hover:bg-gray-300 border-b-1"
-        v-clipboard="item.href">
-        <img :src="item.href" :key="item.text" :alt="item.text" class="w-auto h-full">
+      <a class="flex items-center justify-between transition border-gray-300 border-solid group link link-info user hover:bg-gray-200 border-b-1"
+        v-clipboard="item.href" :key="item.text">
+        <img :src="item.href" :key="item.text" :alt="item.text" class="w-auto h-full p-3 bg-gray-100 rounded-md">
+        <span class="absolute left-[50%] group-hover:block hidden text-gray-500"> click to copy image url</span>
         <span class="text-blue-500 text-[30px]">
           {{ item.text }}
         </span>
